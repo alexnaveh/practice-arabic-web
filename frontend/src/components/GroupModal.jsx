d69936809afda806058cf966bb4536203dc5a1b8
alex <alexzhivot@gmail.com>
2026-03-22 06:56:13 +0200

    remove: auto complete when choosing group name

--- frontend/src/components/GroupModal.jsx
+++ frontend/src/components/GroupModal.jsx
@@ -33,6 +33,7 @@ function NewGroupForm({ onSubmit, onClose }) {
           name="name"
           type="text"
           placeholder="Group name"
+          autoComplete="off"
           autoFocus
           className="w-full border border-gray-300 rounded px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-300"
         />
